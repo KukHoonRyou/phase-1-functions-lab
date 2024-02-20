1dd7e66f299635a73db3d1cfe4d783c94af21580
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-20 01:08:41 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42;
+    }
+    if (blocks < 42) {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    }
+    if (destination < start) {
+        return (start - destination) * 264;
+    }
+
+}
+/*
+function calculatesFarePrice(start, destination) {
+    const distance = distanceFromHqInFeet(start, destination);
+    
+    if (distance <= 400) {
+        return 0;
+    }
+    if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    }
+    if (distance > 2000 && distance <= 2500) {
+        return 25;
+    }
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
+*/
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } 
+    if (distance > 400 && distance <= 2000) {
+      return 0.02 * (distance - 400);
+    } 
+    if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } 
+    if (distance > 2500) {
+      return 'cannot travel that far';
+    }
+  }
 // Code your solution in this file!
